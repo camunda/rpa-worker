9deba4f91bbfa1f16d3d39eed8982b4e5b2a8fa1
Adam Pounder <adam.pounder@camunda.com>
2025-01-30 19:07:02 +0000

    Fix workspace cleanup

--- src/main/java/io/camunda/rpa/worker/robot/WorkspaceService.java
+++ src/main/java/io/camunda/rpa/worker/robot/WorkspaceService.java
@@ -5,55 +5,62 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.nio.file.Path;
 import java.util.Queue;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class WorkspaceService {
+
+	private static final Scheduler cleanupScheduler = Schedulers.newSingle("cleanup");
 	
 	private final IO io;
 	
 	private final Queue<Path> preserveLastQueue = new ConcurrentLinkedQueue<>();
 	private final Queue<Path> reapQueue = new ConcurrentLinkedQueue<>();
-	private final ExecutorService executorService = Executors.newSingleThreadExecutor(r -> {
-		Thread thread = Executors.defaultThreadFactory().newThread(r);
-		thread.setUncaughtExceptionHandler((throwingThread, thrown) -> log.atError()
-				.kv("thread", throwingThread.getName())
-				.setCause(thrown)
-				.log("Exception in workspace cleanup thread"));
-		thread.setName("cleanup");
-		return thread;
-	});
 
-	/// Unconditionally delete a workspace. The deletion is scheduled **immediately**, and the returned 
-	/// signal-only Mono is already hot, even if it is not subscribed to.
+
+	/// Unconditionally delete a workspace. 
+	/// The deletion is scheduled **immediately**; the returned {@link Mono} is used only to signal that a clean-up task 
+	/// has ran, and does not need to be subscribed to for the deletion to take place.
 	/// 
 	/// @param workspace workspace to delete
-	/// @return A hot signal-only Mono signalling completion of the deletion
+	/// @return A signal-only Mono signalling completion of the clean-up task
 	public Mono<Void> deleteWorkspace(Path workspace) {
 		reapQueue.add(workspace);
 		return run();
 	}
 
-	/// Delete a workspace when it is superseded as the last workspace used. The deletion is scheduled 
-	/// **immediately**, and the returned signal-only Mono is already hot, even if it is not subscribed to.
+	/// Delete a workspace when it is superseded as the last workspace used. 
+	/// The deletion is scheduled **immediately**; the returned {@link Mono} is used only to signal that a clean-up task 
+	/// has ran, and does not need to be subscribed to for the deletion to take place. Note that the clean-up task for 
+	/// `preserveLast` will not delete the workspace passed in by the argument, as that will become the last one and will
+	/// wait to be superseded. 
 	///
 	/// @param workspace workspace to delete
-	/// @return A hot signal-only Mono signalling completion of the deletion
+	/// @return A signal-only Mono signalling completion of the clean-up task
 	public Mono<Void> preserveLast(Path workspace) {
 		preserveLastQueue.add(workspace);
 		return run();
 	}
 	
+	@SuppressWarnings("CallingSubscribeInNonBlockingScope")
 	private Mono<Void> run() {
-		return Mono.fromCompletionStage(CompletableFuture.runAsync(this::doRun, executorService));
+		Mono<Void> cleanupTask = Mono.fromRunnable(this::doRun)
+				.subscribeOn(cleanupScheduler)
+				.then()
+				.doOnError(thrown -> log.atError()
+						.setCause(thrown)
+						.log("Error cleaning up workspaces"))
+				.onErrorComplete()
+				.cache();
+		cleanupTask.subscribe();
+		return cleanupTask;
 	}
 
 	private void doRun() {
