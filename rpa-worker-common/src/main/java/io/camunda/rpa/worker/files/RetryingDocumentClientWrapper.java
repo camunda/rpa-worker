9423a9ceada30e6a3720754470bddab49c7e90e8
Adam Pounder <adam.pounder@camunda.com>
2025-02-20 14:30:37 +0000

    Retry document requests to Zeebe

---
+++ rpa-worker-common/src/main/java/io/camunda/rpa/worker/files/RetryingDocumentClientWrapper.java
@@ -0,0 +1,40 @@
+package io.camunda.rpa.worker.files;
+
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpEntity;
+import org.springframework.util.MultiValueMap;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Slf4j
+public class RetryingDocumentClientWrapper implements DocumentClient {
+	
+	private static final Retry retrySpec = Retry
+			.backoff(3, Duration.ofSeconds(1))
+			.filter(thrown -> ! (thrown instanceof FeignException.FeignClientException))
+			.doBeforeRetry(signal -> log.atError()
+					.setCause(signal.failure())
+					.log("Error during document request to Zeebe, retrying"));
+	
+	private final DocumentClient delegate;
+	
+	@Override
+	public Flux<DataBuffer> getDocument(String documentId, String storeId, String contentHash) {
+		return delegate.getDocument(documentId, storeId, contentHash)
+				.retryWhen(retrySpec);
+	}
+
+	@Override
+	public Mono<ZeebeDocumentDescriptor> uploadDocument(MultiValueMap<String, HttpEntity<?>> data, Map<String, String> query) {
+		return delegate.uploadDocument(data, query)
+				.retryWhen(retrySpec);
+	}
+}
