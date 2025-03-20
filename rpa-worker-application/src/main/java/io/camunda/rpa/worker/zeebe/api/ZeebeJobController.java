7649aef5098167e596f47aa982c5feccc55a5d59
Adam Pounder <adam.pounder@camunda.com>
2025-03-20 20:45:59 +0000

    wip

--- rpa-worker-application/src/main/java/io/camunda/rpa/worker/zeebe/api/ZeebeJobController.java
+++ rpa-worker-application/src/main/java/io/camunda/rpa/worker/zeebe/api/ZeebeJobController.java
@@ -1,11 +1,15 @@
 package io.camunda.rpa.worker.zeebe.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.camunda.rpa.worker.zeebe.ZeebeClientStatus;
 import io.camunda.rpa.worker.zeebe.ZeebeJobService;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.command.ThrowErrorCommandStep1;
+import io.vavr.control.Try;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,11 +27,22 @@ class ZeebeJobController {
 	
 	private final ZeebeClient zeebeClient;
 	private final ZeebeJobService zeebeJobService;
+	private final ZeebeClientStatus zeebeClientStatus;
+	private final ObjectMapper objectMapper;
 	
 	@PostMapping("{jobKey}/throw")
-	public Mono<ResponseEntity<Void>> throwError(
+	public Mono<ResponseEntity<?>> throwError(
 			@PathVariable long jobKey, 
 			@RequestBody @Valid JobThrowErrorRequest request) {
+		
+		if( ! zeebeClientStatus.isZeebeClientEnabled())
+			return Mono.just(ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+					.header("Content-type", "text/plain")
+					.body("""
+		*** STUB: zeebe/newThrowErrorCommand ***
+		Would have issued this command to Zeebe:
+		%s
+		""".formatted(Try.of(() -> objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(request)).get())));
 
 		ThrowErrorCommandStep1.ThrowErrorCommandStep2 builder = zeebeClient
 				.newThrowErrorCommand(jobKey)
