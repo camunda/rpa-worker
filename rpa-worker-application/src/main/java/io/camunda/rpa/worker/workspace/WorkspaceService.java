12d1f7b8535481bdb09c2f5f55efe49ce4ad76c2
Adam Pounder <adam.pounder@camunda.com>
2025-06-25 14:24:02 +0100

    #213 - Lock workspaces dir and recreate if deleted

--- rpa-worker-application/src/main/java/io/camunda/rpa/worker/workspace/WorkspaceService.java
+++ rpa-worker-application/src/main/java/io/camunda/rpa/worker/workspace/WorkspaceService.java
@@ -1,27 +1,38 @@
 package io.camunda.rpa.worker.workspace;
 
 import io.camunda.rpa.worker.io.IO;
+import io.vavr.control.Try;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WorkspaceService {
 
 	private final IO io;
 
 	private final Map<String, Workspace> workspaces = new ConcurrentHashMap<>();
+	private final Set<LockableFileChannel> locks = new CopyOnWriteArraySet<>();
 	
 	@Getter(AccessLevel.PACKAGE)
 	private Path workspacesDir;
@@ -29,6 +40,7 @@ public class WorkspaceService {
 	@PostConstruct
 	WorkspaceService init() {
 		workspacesDir = io.createTempDirectory("rpa-workspaces");
+		ensureWorkspacesDirExistsAndIsLocked();
 		return this;
 	}
 
@@ -42,6 +54,7 @@ public class WorkspaceService {
 
 
 	public Workspace createWorkspace(String affinityKey, Map<String, Object> properties) {
+		ensureWorkspacesDirExistsAndIsLocked();
 		Path workspacePath = io.createTempDirectory(workspacesDir, "workspace");
 		String workspaceID = workspacePath.getFileName().toString();
 		io.writeString(workspacePath.resolve(".workspace"), workspaceID, StandardOpenOption.CREATE_NEW);
@@ -86,5 +99,32 @@ public class WorkspaceService {
 						io.size(wr.requestedFile()),
 						wr.requestedFile()));
 	}
-
+	
+	private record LockableFileChannel(FileChannel fileChannel, FileLock fileLock) {
+		static LockableFileChannel locking(Path path) {
+			return Try.of(() -> new RandomAccessFile(path.toFile(), "rw"))
+					.mapTry(fh -> new LockableFileChannel(fh.getChannel(), fh.getChannel().lock()))
+					.get();
+		}
+	}
+	
+	private void ensureWorkspacesDirExistsAndIsLocked() {
+		io.createDirectories(workspacesDir);
+		Path lockFile = workspacesDir.resolve(".lock");
+		Try.of(() -> LockableFileChannel.locking(lockFile))
+				.onSuccess(locks::add)
+				.recover(OverlappingFileLockException.class, _ -> null)
+				.onFailure(thrown -> log.atError()
+						.setCause(thrown)
+						.kv("lockFile", lockFile)
+						.log("Couldn't lock workspaces dir"));
+	}
+	
+	@PreDestroy
+	private void destroy() {
+		locks.forEach(lock -> {
+			Try.run(() -> lock.fileLock().release());
+			Try.run(() -> lock.fileChannel().close());
+		});
+	}
 }
