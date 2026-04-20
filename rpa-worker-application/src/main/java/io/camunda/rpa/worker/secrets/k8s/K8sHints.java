89ecdaaf0546ceee1d568e87d6d541163936099f
Adam Pounder <adam.pounder@camunda.com>
2026-04-20 15:23:46 +0100

    Secrets - Add Kubernetes as secrets backend

---
+++ rpa-worker-application/src/main/java/io/camunda/rpa/worker/secrets/k8s/K8sHints.java
@@ -0,0 +1,24 @@
+package io.camunda.rpa.worker.secrets.k8s;
+
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+import java.util.stream.Stream;
+
+@Configuration
+@ImportRuntimeHints(K8sHints.class)
+class K8sHints implements RuntimeHintsRegistrar {
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		Stream.of(ObjectMeta.class)
+				.forEach(klass -> hints
+						.reflection()
+						.registerType(
+								klass,
+								MemberCategory.values()));
+	}
+}
